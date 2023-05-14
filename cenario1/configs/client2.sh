52fe78d99aef4236b8727bf5ebe07cc523e02814
Luis <2162357@my.ipleiria.pt>
2023-05-14 22:18:42 +0100

    correct IPs on linux clients

--- cenario1/configs/client2.sh
+++ cenario1/configs/client2.sh
@@ -4,7 +4,7 @@ apt update
 #apt upgrade
 apt install vim nano net-tools traceroute iproute2 netscript-2.4 iputils-ping telnet iperf3 openssh-server zabbix-agent -y
 
-ip addr add 10.20.20.25/24 dev eth1
+ip addr add 10.20.20.22/24 dev eth1
 ip route add 10.10.10.0/24 via 10.20.20.1
 ip route add 1.1.1.1/32 via 10.10.10.1
 ip route add 2.2.2.2/32 via 10.10.10.1
